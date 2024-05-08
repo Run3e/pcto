c459aec565076e210723a3b6dc12f1a7e926e9eb
Run3e <169042591+Run3e@users.noreply.github.com>
2024-05-08 09:51:49 +0000

    sdfs

--- ex07.c
+++ ex07.c
@@ -2,14 +2,19 @@
 
 int main()
 {
-    int b=0;   //Per far inserire all'utente una serie di numeri (100 in questo caso)
-    int num2[100];
+    int b=0;   //Per far inserire all'utente una serie di numeri (10 in questo caso)
+    int num2[10];
 
-    while (b<100)
+    while (b<10)
     {
         printf("inserisci numero\n");
         scanf("%d", &num2[b]);
 
         b=b+1;
     }
+
+    char nomeutente[50]; //Per far inserire al giocatore il proprio nome
+    
+    printf("Inserire nome utente\n");
+    scanf("%s", nomeutente); //Con le stringe non ci va la &
 }
--- game.c
+++ game.c
@@ -2,5 +2,23 @@
 
   int main()
   {
+
+    int choice;
+
+    char nomeutente[50];
+    printf("Inserire nome ");
+    scanf("%s", nomeutente);
+    printf("Benvenuto %s. \n", nomeutente);
+    printf("Sono le 16 e sei appena uscito da scuola, siccome abiti lontano e hai una stazione vicino scuola decidi di aspettare lì il treno");
+    printf("Cosa fai?\n1) Aspetti il treno\n2) Chiami i tuoi genitori (Scrivere il numero della scelta)");
+    scanf("%d", &choice);
     
+    if(choice<=1)
+    {
+      printf("Raggiungi la stazione e ti siedi su una panchina");
+    }
+    else
+    {
+      printf("Nonostante lasci squillare il telefono a lungo non risponde nessuno...\nDecidi quindi di rassegnarti e aspettare il treno");
+    }
   }
